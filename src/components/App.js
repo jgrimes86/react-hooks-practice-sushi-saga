543b3f78b8e2edd2754cf9d487414c6738b1550a
Jim Grimes <jimgrimes86@gmail.com>
2023-10-07 15:05:40 -0400

    DONE: displays 4 sushis, eat sushi, show empty plates, and reduce money

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [availableSushi, setAvailableSushi] = useState([]);
+  const [plates, setPlates] = useState([])
+  const [money, setMoney] = useState(50)
+
+  useEffect(() => {
+    fetch(API)
+    .then(response => response.json())
+    .then(sushi => setAvailableSushi(sushi))
+    }, [])
+
+  function eatSushi (name, price) {
+    if (money > price) {
+      return (
+        setPlates((plates) => [...plates, name]),
+        setMoney((money) => money-price)
+      )
+    }
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer availableSushi={availableSushi} eatSushi={eatSushi} money={money} />
+      <Table plates={plates} money={money} />
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
 function SushiContainer(props) {
+  const {availableSushi, eatSushi, money} = props;
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const fourSushis = availableSushi.slice(currentIndex, 4)
+  
+  const sushiDisplay = fourSushis.map((sushi) => {
+    return <Sushi key={sushi.id} sushi={sushi} eatSushi={eatSushi} money={money} />
+  })
+  // console.log(fourSushis)
+
+  function moreSushi() {
+    // setCurrentIndex((currentIndex) => currentIndex+4);
+    console.log('clicked')
+  }
+  // console.log("current index: ", currentIndex)
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiDisplay}
+      <MoreButton moreSushi={moreSushi}/>
     </div>
   );
 }
