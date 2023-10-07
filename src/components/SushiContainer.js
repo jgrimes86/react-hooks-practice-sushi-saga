5bb133249fdce19826f56a329519cd81934bc40e
Jim Grimes <jimgrimes86@gmail.com>
2023-10-07 15:40:59 -0400

    DONE: more sushi button shows next 4 sushi in database

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import MoreButton from "./MoreButton";
 import Sushi from "./Sushi";
 
@@ -6,18 +6,16 @@ function SushiContainer(props) {
   const {availableSushi, eatSushi, money} = props;
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const fourSushis = availableSushi.slice(currentIndex, 4)
+  function moreSushi() {
+    setCurrentIndex((currentIndex) => currentIndex+4);
+  }
+  console.log("current index: ", currentIndex)
+
+  const fourSushis = availableSushi.slice(currentIndex, currentIndex+4)
   
   const sushiDisplay = fourSushis.map((sushi) => {
     return <Sushi key={sushi.id} sushi={sushi} eatSushi={eatSushi} money={money} />
   })
-  // console.log(fourSushis)
-
-  function moreSushi() {
-    // setCurrentIndex((currentIndex) => currentIndex+4);
-    console.log('clicked')
-  }
-  // console.log("current index: ", currentIndex)
 
   return (
     <div className="belt">
